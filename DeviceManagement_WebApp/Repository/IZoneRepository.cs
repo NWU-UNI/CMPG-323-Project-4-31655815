85810f550408dc6e02e3feb36524c319eeab080c
PJ Van Staden <pjvanstaden.pvs.@gmail.com>
2022-09-24 23:57:13 +0200

    Connection update

---
+++ DeviceManagement_WebApp/Repository/IZoneRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Policy;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public interface IZoneRepository : IGenericRepository<Zone>
+    {
+        Zone GetMostRecentZone();
+    }
+
+
+}
--- DeviceManagement_WebApp/Repository/ZoneRepository.cs
+++ DeviceManagement_WebApp/Repository/ZoneRepository.cs
@@ -1,6 +1,16 @@
 ﻿namespace DeviceManagement_WebApp.Repository
 {
-    public class ZoneRepository
+    public class ZoneRepository : GenericRepository<Zone>, IZoneRepository
     {
     }
-}
+        public ServiceRepository(CMPG323Context context) : base(context)
+        {
+        }
+
+        public Zone GetMostRecentZone()
+        {
+            return _context.Zone.OrderByDescending(Zone => Zone.CreatedDate).FirstOrDefault();
+        }
+
+
+    }
